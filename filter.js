4db71b58b34ab89f3ecd6748d493166617c660ef
M. OK <okeeffeme@gmail.com>
2024-09-06 17:57:33 +0200

    Improving filter, bug with set

--- filter.js
+++ filter.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const checkboxes = document.querySelectorAll('form[id=testForm]>input[type=checkbox]');
-    console.log(checkboxes.length)
-    for (let i = 0; i < checkboxes.length; i++) {
-        console.log(checkboxes[i])
-        checkboxes[i].addEventListener('change', (e) => {
-            console.log('change', e.target.checked, e.target.name)
+    const posts = document.getElementsByClassName('post');
+    let currentSelection = new Set();
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', (e) => {
+            currentSelection = e.target.checked ? 
+            currentSelection.add(e.target.name) : 
+            currentSelection.delete(e.target.name);
+            console.log(typeof currentSelection, currentSelection)
+            togglePost(currentSelection)
+        })
+    })
+
+    function togglePost(currentSelection) {
+        console.log(typeof currentSelection, currentSelection)
+        currentSelection.forEach((current) => {
+            //must use for loop, HTMLCollection does not have forEach
+            for (let i = 0; i < posts.length; i++) {
+                if (posts[i].id.includes(current)) {
+                    posts[i].style.backgroundColor = 'green';
+                } else {
+                    posts[i].style.backgroundColor = 'red';
+                    // posts[i].style.display = '';
+                }
+            }
         })
     }
   });   
